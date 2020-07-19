2093eb9c95aeb5da34064ddb7404f79e9f9da675
Ross Kuyper <ross@rosskuyper.com>
2020-07-19 17:25:35 +0200

    Pull event marshalling out into util

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,7 @@
 import './utils/bugsnag'
 import {ApolloServer, gql} from 'apollo-server-lambda'
-import queryString from 'query-string'
-import {
-  APIGatewayProxyCallback,
-  APIGatewayProxyEvent,
-  APIGatewayProxyEventV2,
-  Context as LambdaContext,
-} from 'aws-lambda'
+import {APIGatewayProxyCallback, APIGatewayProxyEventV2, Context as LambdaContext} from 'aws-lambda'
+import {marshallLambdaEvent} from './utils/apigwProxy'
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
@@ -35,90 +30,6 @@ const server = new ApolloServer({
   introspection: true,
 })
 
-type SingleValueObject = {
-  [name: string]: string
-}
-
-type MultiValueObject = {
-  [name: string]: string[]
-}
-
-type SingleOrMultiValueObject = {
-  [name: string]: string[] | string | null | undefined
-}
-
-const parseMultiValueHeaders = (headers: SingleValueObject): MultiValueObject => {
-  const headerPairs = Object.keys(headers).map((headerKey) => {
-    return [headerKey, [headers[headerKey]]]
-  })
-
-  return Object.fromEntries(headerPairs)
-}
-
-const ensureSingleQueryParams = (parsedQueryParams: SingleOrMultiValueObject): SingleValueObject => {
-  const queryPairs = Object.keys(parsedQueryParams).map((paramKey) => {
-    const param = parsedQueryParams[paramKey]
-
-    return [paramKey, Array.isArray(param) ? param[0] : param]
-  })
-
-  return Object.fromEntries(queryPairs)
-}
-
-const ensureMultiQueryParams = (parsedQueryParams: SingleOrMultiValueObject): MultiValueObject => {
-  const queryPairs = Object.keys(parsedQueryParams).map((paramKey) => {
-    const param = parsedQueryParams[paramKey]
-
-    return [paramKey, Array.isArray(param) ? param : [param]]
-  })
-
-  return Object.fromEntries(queryPairs)
-}
-
-const marshallLambdaEvent = (event: APIGatewayProxyEventV2): APIGatewayProxyEvent => {
-  const parsedQueryParams = event.rawQueryString ? queryString.parse(event.rawQueryString) : null
-
-  return {
-    body: event.body || null,
-    headers: event.headers,
-    multiValueHeaders: parseMultiValueHeaders(event.headers),
-    httpMethod: event.requestContext.http.method,
-    isBase64Encoded: event.isBase64Encoded,
-    path: event.requestContext.http.path,
-    pathParameters: null,
-    queryStringParameters: parsedQueryParams ? ensureSingleQueryParams(parsedQueryParams) : null,
-    multiValueQueryStringParameters: parsedQueryParams ? ensureMultiQueryParams(parsedQueryParams) : null,
-    stageVariables: null,
-    requestContext: {
-      ...event.requestContext,
-      httpMethod: event.requestContext.http.method,
-      protocol: event.requestContext.http.method,
-      path: event.requestContext.http.path,
-      requestTimeEpoch: event.requestContext.timeEpoch,
-      resourcePath: event.requestContext.http.path,
-      resourceId: event.requestContext.routeKey,
-      authorizer: {},
-      identity: {
-        accessKey: null,
-        accountId: null,
-        apiKey: null,
-        apiKeyId: null,
-        caller: null,
-        cognitoAuthenticationProvider: null,
-        cognitoAuthenticationType: null,
-        cognitoIdentityId: null,
-        cognitoIdentityPoolId: null,
-        principalOrgId: null,
-        sourceIp: event.requestContext.http.sourceIp,
-        user: null,
-        userAgent: null,
-        userArn: null,
-      },
-    },
-    resource: event.routeKey,
-  }
-}
-
 const handler = server.createHandler({
   cors: {
     origin: '*',
@@ -126,6 +37,6 @@ const handler = server.createHandler({
   },
 })
 
-exports.handler = (event: any, context: LambdaContext, callback: APIGatewayProxyCallback) => {
+exports.handler = (event: APIGatewayProxyEventV2, context: LambdaContext, callback: APIGatewayProxyCallback) => {
   return handler(marshallLambdaEvent(event), context, callback)
 }
---
+++ src/utils/apigwProxy.ts
@@ -0,0 +1,117 @@
+/**
+ * The simple and convenient setup of api gateway <-> lambda via terraform for their HTTP / WebSocket
+ * integration uses a new event payload format.
+ *
+ * The apollo lambda integration does not seem to support this payload yet, so here we marshall / convert
+ * it to the older format.
+ *
+ * This is not ideal, but because the bulk of GQL request handling is dependent on the request body it's
+ * unlikely that any issues will be caused by this.
+ *
+ * For a proper production environment it would be better to get the support for the new payload version
+ * into apollo.
+ */
+import queryString, {ParsedQuery} from 'query-string'
+import {APIGatewayProxyEvent, APIGatewayProxyEventV2} from 'aws-lambda'
+
+type SingleDimensionObject = {[name: string]: string}
+type TwoDimensionObject = {[name: string]: string[]}
+
+// Convert 1D headers into a 2D array
+const parseMultiValueHeaders = (headers: SingleDimensionObject): TwoDimensionObject => {
+  const headerPairs = Object.keys(headers).map((headerKey) => {
+    return [headerKey, [headers[headerKey]]]
+  })
+
+  return Object.fromEntries(headerPairs)
+}
+
+const parseQueryParams = (parsedQueryParams: ParsedQuery<string>): ParsedQueryParams => {
+  const queryStringParameters: SingleDimensionObject = {}
+  const multiValueQueryStringParameters: TwoDimensionObject = {}
+
+  Object.keys(parsedQueryParams).forEach((paramKey) => {
+    const param = parsedQueryParams[paramKey]
+
+    // Filter out null and undefined values which queryString.parse might give us
+    if (param === undefined || param === null) {
+      return
+    }
+
+    // Add the dimension-adjusted value to both objects
+    if (Array.isArray(param)) {
+      queryStringParameters[paramKey] = param.join(',')
+      multiValueQueryStringParameters[paramKey] = param
+    } else {
+      queryStringParameters[paramKey] = param
+      multiValueQueryStringParameters[paramKey] = [param]
+    }
+  })
+
+  return {
+    queryStringParameters,
+    multiValueQueryStringParameters,
+  }
+}
+
+type ParsedQueryParams = {
+  queryStringParameters: SingleDimensionObject | null
+  multiValueQueryStringParameters: TwoDimensionObject | null
+}
+
+const parseRawQueryString = (rawQueryString: string): ParsedQueryParams => {
+  const parsedQueryParams = queryString.parse(rawQueryString)
+
+  if (!parsedQueryParams) {
+    return {
+      queryStringParameters: null,
+      multiValueQueryStringParameters: null,
+    }
+  }
+
+  return parseQueryParams(parsedQueryParams)
+}
+
+export const marshallLambdaEvent = (event: APIGatewayProxyEventV2): APIGatewayProxyEvent => {
+  const {queryStringParameters, multiValueQueryStringParameters} = parseRawQueryString(event.rawQueryString)
+
+  return {
+    body: event.body || null,
+    headers: event.headers,
+    multiValueHeaders: parseMultiValueHeaders(event.headers),
+    httpMethod: event.requestContext.http.method,
+    isBase64Encoded: event.isBase64Encoded,
+    path: event.requestContext.http.path,
+    pathParameters: null,
+    queryStringParameters,
+    multiValueQueryStringParameters,
+    stageVariables: null,
+    requestContext: {
+      ...event.requestContext,
+      httpMethod: event.requestContext.http.method,
+      protocol: event.requestContext.http.method,
+      path: event.requestContext.http.path,
+      requestTimeEpoch: event.requestContext.timeEpoch,
+      resourcePath: event.requestContext.http.path,
+      resourceId: event.requestContext.routeKey,
+      authorizer: {},
+      identity: {
+        accessKey: null,
+        accountId: null,
+        apiKey: null,
+        apiKeyId: null,
+        caller: null,
+        cognitoAuthenticationProvider: null,
+        cognitoAuthenticationType: null,
+        cognitoIdentityId: null,
+        cognitoIdentityPoolId: null,
+        principalOrgId: null,
+        sourceIp: event.requestContext.http.sourceIp,
+        user: null,
+        userAgent: null,
+        userArn: null,
+      },
+    },
+    resource: event.routeKey,
+  }
+}
