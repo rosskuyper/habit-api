0a75c24c3b83f198aaf0cd6640182086ebe0be86
Ross Kuyper <ross@rosskuyper.com>
2020-07-19 09:57:41 +0200

    Add Typescript and CD deploy

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
-const { ApolloServer, gql } = require('apollo-server-lambda');
-const queryString = require('query-string')
+import { ApolloServer, gql } from 'apollo-server-lambda'
+import queryString from 'query-string'
+import {
+    APIGatewayProxyCallback,
+    APIGatewayProxyEvent,
+    APIGatewayProxyEventV2,
+    Context as LambdaContext,
+  } from 'aws-lambda'
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
@@ -28,15 +34,27 @@ const server = new ApolloServer({
   introspection: true,
 });
 
-const parseMultiValueHeaders = (headers) => {
+type SingleValueObject = {
+    [name: string]: string
+}
+
+type MultiValueObject = {
+    [name: string]: string[]
+}
+
+type SingleOrMultiValueObject = {
+    [name: string]: string[] | string | null | undefined
+}
+
+const parseMultiValueHeaders = (headers: SingleValueObject): MultiValueObject => {
     const headerPairs = Object.keys(headers).map((headerKey) => {
-        return [headerKey, headers[headerKey]]
+        return [headerKey, [headers[headerKey]]]
     })
 
     return Object.fromEntries(headerPairs)
 }
 
-const ensureSingleQueryParams = (parsedQueryParams) => {
+const ensureSingleQueryParams = (parsedQueryParams: SingleOrMultiValueObject): SingleValueObject => {
     const queryPairs = Object.keys(parsedQueryParams).map((paramKey) => {
         const param = parsedQueryParams[paramKey]
 
@@ -49,7 +67,7 @@ const ensureSingleQueryParams = (parsedQueryParams) => {
     return Object.fromEntries(queryPairs)
 }
 
-const ensureMultiQueryParams = (parsedQueryParams) => {
+const ensureMultiQueryParams = (parsedQueryParams: SingleOrMultiValueObject): MultiValueObject => {
     const queryPairs = Object.keys(parsedQueryParams).map((paramKey) => {
         const param = parsedQueryParams[paramKey]
 
@@ -62,7 +80,7 @@ const ensureMultiQueryParams = (parsedQueryParams) => {
     return Object.fromEntries(queryPairs)
 }
 
-const marshallLambdaEvent = (event) => {
+const marshallLambdaEvent = (event: APIGatewayProxyEventV2): APIGatewayProxyEvent => {
     const parsedQueryParams = event.rawQueryString ? queryString.parse(event.rawQueryString) : null
 
     return {
@@ -76,13 +94,43 @@ const marshallLambdaEvent = (event) => {
         queryStringParameters: parsedQueryParams ? ensureSingleQueryParams(parsedQueryParams) : null,
         multiValueQueryStringParameters: parsedQueryParams ? ensureMultiQueryParams(parsedQueryParams) : null,
         stageVariables: null,
-        requestContext: event.requestContext,
+        requestContext: {
+            ...event.requestContext,
+            httpMethod: event.requestContext.http.method,
+            protocol: event.requestContext.http.method,
+            path: event.requestContext.http.path,
+            requestTimeEpoch: event.requestContext.timeEpoch,
+            resourcePath: event.requestContext.http.path,
+            resourceId: event.requestContext.routeKey,
+            authorizer: {},
+            identity: {
+                accessKey: null,
+                accountId: null,
+                apiKey: null,
+                apiKeyId: null,
+                caller: null,
+                cognitoAuthenticationProvider: null,
+                cognitoAuthenticationType: null,
+                cognitoIdentityId: null,
+                cognitoIdentityPoolId: null,
+                principalOrgId: null,
+                sourceIp: event.requestContext.http.sourceIp,
+                user: null,
+                userAgent: null,
+                userArn: null,
+            },
+        },
         resource: event.routeKey,
     }
 }
 
-const handler = server.createHandler();
+const handler = server.createHandler({
+    cors: {
+      origin: '*',
+      credentials: true,
+    },
+  })
 
-exports.handler = (event, context, callback) => {
+exports.handler = (event: any, context: LambdaContext, callback: APIGatewayProxyCallback) => {
     return handler(marshallLambdaEvent(event), context, callback)
 }
