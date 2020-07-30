6bfb8dcc699badc901dff7199e84fd320a2e723d
Ross Kuyper <ross@rosskuyper.com>
2020-07-30 13:45:38 +0200

    Trim cors origins config

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,8 @@
 import env from 'env-var'
 
+// env-var does not trim values pulled as arrays by default
+const trim = (item: string) => item.trim()
+
 export const NODE_ENV = env.get('NODE_ENV').required().asString()
 export const IS_LOCAL = NODE_ENV === 'local'
 
@@ -17,4 +20,4 @@ export const AUTH_COGNITO_CLIENT_SECRET = env.get('AUTH_COGNITO_CLIENT_SECRET').
 export const AUTH_COGNITO_AUTHORIZE_DOMAIN = env.get('AUTH_COGNITO_AUTHORIZE_DOMAIN').required().asString()
 export const AUTH_COGNITO_REDIRECT_URI = env.get('AUTH_COGNITO_REDIRECT_URI').required().asString()
 
-export const CORS_ALLOWED_ORIGINS = env.get('CORS_ALLOWED_ORIGINS').required().asArray()
+export const CORS_ALLOWED_ORIGINS = env.get('CORS_ALLOWED_ORIGINS').required().asArray().map(trim)
