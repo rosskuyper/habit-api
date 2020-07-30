456d646d51f7a87ec5a2014a876a9e8123eb00e9
Ross Kuyper <ross@rosskuyper.com>
2020-07-30 18:41:16 +0200

    Remove logs

--- src/handler.ts
+++ src/handler.ts
@@ -27,8 +27,5 @@ const getAppServerBundle = deferPromiseCall(
 export async function handler(event: APIGatewayProxyEventV2, context: LambdaContext): Promise<Response> {
   const {serverlessServer} = await getAppServerBundle()
 
-  console.log('event', event)
-  console.log('marshalledEvent', marshallLambdaEvent(event))
-
   return awsServerlessExpress.proxy(serverlessServer, marshallLambdaEvent(event), context, 'PROMISE').promise
 }
