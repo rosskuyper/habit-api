2abbb3c11eb76aeceb1a0c97a90958369791c04d
Ross Kuyper <ross@rosskuyper.com>
2020-07-30 12:04:11 +0200

    v12 formatting

--- infra/secrets.tf
+++ infra/secrets.tf
@@ -3,7 +3,7 @@ data "aws_kms_secrets" "main" {
     name    = "bugsnag_api_key"
     payload = "AQICAHhOyM5btVbfvpZQ/4Z6nIPAoZ4eCTd0WLRyE39dZQLJxAE1MkkhMZs3mVzZglCsiRwqAAAAfjB8BgkqhkiG9w0BBwagbzBtAgEAMGgGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQM5EqBu7DxJ5/IO/cIAgEQgDs3rqvPc+hnnE+G61TjEfnsDNfA9IcLVVNl93lWn7FAdp/jWwFnyDoBQvSIekjF5VIvIEdMuQ3fNvySAg=="
 
-    context {
+    context = {
       service = local.service_name
     }
   }
